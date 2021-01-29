f35fc073383d77cb3aa9297732fb8ee9e9c2cd37
tiago-rodrigues1 <tiagords127@gmail.com>
2021-01-28 22:03:45 -0300

    ajusta output do 1 exercicio da aula 02

--- src/exercicio02.java
+++ src/exercicio02.java
@@ -9,7 +9,6 @@ public class exercicio02 {
 		
 		System.out.print("Digite o valor do raio: ");
 		raio = javaIn.nextDouble();
-		System.out.print("\n");
 		
 		double diametro, perimetro, area;
 		
@@ -17,7 +16,7 @@ public class exercicio02 {
 		perimetro = diametro * pi;
 		area = pi * (raio*raio);
 		
-		System.out.println("Para raio = " + raio + ", temos:");
+		System.out.println("Para raio = " + raio + " ->\n");
 		System.out.printf("Diametro = %.2f\n", diametro);
 		System.out.printf("Perimetro = %.2f\n", perimetro);
 		System.out.printf("Area = %.2f\n", area);
